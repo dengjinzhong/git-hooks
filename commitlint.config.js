ee853167c8fe0ca92013f040565b87fd1b56a898
dengjinhong <hn.dengjinzhong@aisino.com>
2022-07-20 14:00:00 +0800

    bug(dsd): 修复一些文档问题

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-empty": [2, 'always']
+    "scope-empty": [2, 'never'],
+    "type-enum": [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'bug']]
   }
 };
